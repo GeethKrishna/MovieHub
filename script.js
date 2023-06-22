6fb685ff59e1048fbbe33fab445980b20cfd3095
GeethKrishna <96900928+GeethKrishna@users.noreply.github.com>
2023-06-22 12:11:31 +0530

    Update2script.js

--- script.js
+++ script.js
@@ -5,36 +5,32 @@ const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
 
-async function loadMovies(searchTerm){
+async function loadMovies(searchTerm) {
   const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=b194381`;
-  const res = await fetch(`${URL}`);
+  const res = await fetch(URL);
   const data = await res.json();
 
-  if(data.Response == "True") displayMovieList(data.Search);
+  if (data.Response === "True") displayMovieList(data.Search);
 }
 
-function findMovies(){
-  let searchTerm = (movieSearchBox.value).trim();
-  if(searchTerm.length > 0){
+function findMovies() {
+  let searchTerm = movieSearchBox.value.trim();
+  if (searchTerm.length > 0) {
     searchList.classList.remove('hide-search-list');
     loadMovies(searchTerm);
-  }
-  else{
+  } else {
     searchList.classList.add('hide-search-list');
   }
 }
 
-function displayMovieList(movies){
+function displayMovieList(movies) {
   searchList.innerHTML = "";
-  for(let idx = 0; idx < movies.length; idx++){
+  for (let idx = 0; idx < movies.length; idx++) {
     let movieListItem = document.createElement('div');
     movieListItem.dataset.id = movies[idx].imdbID;
     movieListItem.classList.add('search-list-item');
-    if(movies[idx].Poster != "N/A")
-      moviePoster = movies[idx].Poster;
-    else
-      moviePoster = "no_img_found.jpg";
-    
+    let moviePoster = movies[idx].Poster !== "N/A" ? movies[idx].Poster : "no_img_found.jpg";
+
     movieListItem.innerHTML = `
       <div class="search-item-thumbnail">
           <img src="${moviePoster}">
@@ -49,10 +45,10 @@ function displayMovieList(movies){
   loadMovieDetails();
 }
 
-function loadMovieDetails(){
+function loadMovieDetails() {
   const searchListMovies = searchList.querySelectorAll('.search-list-item');
   searchListMovies.forEach(movie => {
-    movie.addEventListener('click',async () => {
+    movie.addEventListener('click', async () => {
       searchList.classList.add('hide-search-list');
       movieSearchBox.value = "";
       const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=b194381`);
@@ -62,28 +58,26 @@ function loadMovieDetails(){
   });
 }
 
-function displayMovieDetails(details){
+function displayMovieDetails(details) {
   resultGrid.innerHTML = `
     <div class="movie-poster">
-      <img src="${(details.Poster != "N/A") ? details.Poster : "no_img_found.jpg"}" alt="The movie poster">
+      <img src="${details.Poster !== "N/A" ? details.Poster : "no_img_found.jpg"}" alt="The movie poster">
     </div>
     <div class="movie-info">
       <h3 class="movie-title">${details.Title}</h3>
       <ul class="movie-misc-info">
           <li class="year">Year: ${details.Year}</li>
           <li class="rated">Ratings: ${details.Rated}</li>
-          <li class="released">Realeased: ${details.Released}</li>
+          <li class="released">Released: ${details.Released}</li>
       </ul>
       <p class="genre"><b>Genre:</b> ${details.Genre}</p>
-      <p class="writter"><b>Writter:</b> ${details.Writer}</p>
+      <p class="writer"><b>Writer:</b> ${details.Writer}</p>
       <p class="actors"><b>Actors:</b> ${details.Actors}........</p>
       <p class="plot"><b>Plot:</b> ${details.Plot}</p>
       <p class="language"><b>Language:</b> ${details.Language}</p>
-      <p class="language"><b>IMDB Rating:</b> ${details.imdbRating}</p>
-      <p class="language"><b>Meta Score:</b> ${details.Metascore}</p>
+      <p class="imdb-rating"><b>IMDB Rating:</b> ${details.imdbRating}</p>
+      <p class="meta-score"><b>Metascore:</b> ${details.Metascore}</p>
       <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
     </div>
     `;
 }
-
-
